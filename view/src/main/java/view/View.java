9fe7d52948f18a857dc983543a36544bb871a66a
Marine-crty <mamaetlele@hotmail.fr>
2019-05-24 16:35:45 +0200

    On fait un premier test LOL

--- view/src/main/java/view/View.java
+++ view/src/main/java/view/View.java
@@ -1,82 +1,23 @@
 package view;
 
-import java.awt.event.KeyEvent;
 
-import javax.swing.SwingUtilities;
-
-import contract.ControllerOrder;
-import contract.IController;
-import contract.IModel;
-import contract.IView;
-
-/**
- * The Class View.
- *
- * @author Jean-Aymeric Diet
- */
-public final class View implements IView, Runnable {
-
-	/** The frame. */
-	private final ViewFrame viewFrame;
-
-	/**
-	 * Instantiates a new view.
-	 *
-	 * @param model
-	 *          the model
-	 */
-	public View(final IModel model) {
-		this.viewFrame = new ViewFrame(model);
-		SwingUtilities.invokeLater(this);
-	}
-
-	/**
-	 * Key code to controller order.
-	 *
-	 * @param keyCode
-	 *          the key code
-	 * @return the controller order
-	 */
-	protected static ControllerOrder keyCodeToControllerOrder(final int keyCode) {
-		switch (keyCode) {
-			case KeyEvent.VK_G:
-				return ControllerOrder.English;
-			case KeyEvent.VK_F:
-				return ControllerOrder.Francais;
-			case KeyEvent.VK_D:
-				return ControllerOrder.Deutsch;
-			case KeyEvent.VK_I:
-				return ControllerOrder.Indonesia;
-			default:
-				return ControllerOrder.English;
+public final class View {
+	char Tableau[][] = {
+			{'4','5','1'},
+			{'8','7','9'}};
+	
+	String Contenu = "Coucou";
+	
+	public View() {
+		for(int x = 0; x<2; x++) {
+			for(int y = 0; y<3; y++) {
+				System.out.print(Tableau[x][y]);
+			}
+			
+			System.out.println();
 		}
+		
+		System.out.println(Contenu);
 	}
-
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see contract.IView#printMessage(java.lang.String)
-	 */
-	public void printMessage(final String message) {
-		this.viewFrame.printMessage(message);
-	}
-
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see java.lang.Runnable#run()
-	 */
-	public void run() {
-		this.viewFrame.setVisible(true);
-	}
-
-	/**
-	 * Sets the controller.
-	 *
-	 * @param controller
-	 *          the new controller
-	 */
-	public void setController(final IController controller) {
-		this.viewFrame.setController(controller);
-	}
+	
 }
