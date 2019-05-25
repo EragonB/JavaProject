bedfadc7b33dd69d51f0280b89957c91294646d1
Monstre55 <mairesully@hotmail.fr>
2019-05-25 09:26:05 +0200

    Display an image

--- view/src/main/java/view/ViewPanel.java
+++ view/src/main/java/view/ViewPanel.java
@@ -1,8 +1,11 @@
 package view;
 
-import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 class ViewPanel extends JPanel{
@@ -10,6 +13,10 @@ class ViewPanel extends JPanel{
 	public void paintComponent(Graphics g) {
 		
 		g.drawString("iofdjvdkjssiodesoezkdszios"/*String.valueOf(Affiche())*/, 40, 80);
+		Affiche(Tableau);
+		g.drawImage(GetImageTest(), 50, 50, this);
+		GetImageTest();
+
 	}
 	
 	char Tableau[][] = {
@@ -18,14 +25,26 @@ class ViewPanel extends JPanel{
 	
 	char Contenu = 'C';
 	
-	public char Affiche() {
-		/*for(int x = 0; x<2; x++) {
+	public void Affiche(char[][] tableau) {
+		for(int x = 0; x<2; x++) {
 			for(int y = 0; y<3; y++) {
-				return Tableau[x][y];
+				System.out.print(tableau[x][y]);
 			}
-		}*/
+			System.out.println();
+		}
 		 
-		return Contenu;
+		
 	}
 
+	public Image GetImageTest()
+	{
+		try {
+			Image PictureDirt = ImageIO.read(new File("C:\\Users\\maire\\git\\JavaProject\\picture\\Stone.png"));
+			return PictureDirt;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 }
