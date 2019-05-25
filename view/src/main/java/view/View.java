85f5541e7115ee66da7780f9f26608a7c64abd70
Monstre55 <mairesully@hotmail.fr>
2019-05-25 22:31:09 +0200

    Prototype V.1 Send Picture in BDD : name varchar(20), img : mediumblob

--- view/src/main/java/view/View.java
+++ view/src/main/java/view/View.java
@@ -1,9 +1,9 @@
 package view;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.*;
+import java.sql.*;
+import java.util.Vector;
+
 
 
 
@@ -13,11 +13,13 @@ public final class View {
 		SaveTest("Bonjour");
 	}
 	
-	public static void SaveTest(String Images)
+	public static void SaveTest(String Perso)
 	{
 		String url = "jdbc:mysql://localhost/javaproject?useLegacyDatetimeCode=false&serverTimezone=Europe/Paris&useSSL=FALSE";
 		String login = "root";
 		String passwd = "";
+		
+		
 		
 		Connection cn 	= null;
 		Statement st 	= null;
@@ -28,10 +30,23 @@ public final class View {
 			cn = DriverManager.getConnection(url, login, passwd);
 			
 			st = cn.createStatement();
-			System.out.println("Probleme");
-			String sql = "INSERT INTO `JavaImageProto` (`Picture`) VALUES (`" + Images + "`)";
+			
+			String sql = "INSERT INTO `perso` (`persocolo`) VALUES ('Boby La Fouine')";
 			
 			st.executeUpdate(sql);
+			try {
+				sauveIMG("../picture/Wall.png","Wallaaa", cn);
+				chargeIMG("WallBias","../picture/WallBias.png",cn);
+				
+				getAllNames(cn);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			
+
 		}
 		catch (SQLException e)
 		{
@@ -50,5 +65,103 @@ public final class View {
 			}
 		}
 
+	}
+	
+	public static void sauveIMG(String location, String name, Connection cn) throws Exception
+	{
+		File ImageTest = new File(location);
+		FileInputStream istreamImageTest = new FileInputStream(ImageTest);
+		
+		try {
+			PreparedStatement ps = cn.prepareStatement("insert into image (name, img) values (?,?)");
+			try
+			{
+				ps.setString(1, name);
+		        ps.setBinaryStream(2, istreamImageTest, (int) ImageTest.length());
+		        ps.executeUpdate();
+			}
+			finally
+			{
+				ps.close();
+			}
+		}
+		finally
+		{
+			istreamImageTest.close();
+		}
+
+	}
+	
+	
+	public static void chargeIMG(String name, String location, Connection cn) throws Exception
+	{
+	  File monImage = new File(location);
+	  FileOutputStream ostreamImage = new FileOutputStream(monImage);         
+	  try
+	  {
+	    PreparedStatement ps = cn.prepareStatement("select img from image where name=?");
+	    try
+	    {
+	      ps.setString(1, name);
+	      ResultSet rs = ps.executeQuery();
+	      try
+	      {
+	        if(rs.next())
+	        {
+	      	  InputStream istreamImage = rs.getBinaryStream("img");
+	      
+	      	  byte[] buffer = new byte[1024];
+	      	  int length = 0;
+		
+	      	  while((length = istreamImage.read(buffer)) != -1)
+	      	  {
+	      	    ostreamImage.write(buffer, 0, length);
+		  }
+	  	}
+	      }
+	      finally
+	      {
+	        rs.close();
+	      }
+	    }
+	    finally
+	    {
+	      ps.close();
+	    }
+	  }
+	  finally
+	  {
+	    ostreamImage.close();
+	  }
+	}
+	
+	public static Vector getAllNames(Connection cn) throws Exception
+	{
+	  Vector res = new Vector();
+
+	  Statement stmt = cn.createStatement();
+
+	  try
+	  {
+	    ResultSet rset = stmt.executeQuery("select name from image");
+
+	    try
+	    {
+	      while(rset.next())
+	      {
+	        res.add(rset.getString("name"));
+	      }
+	    }
+	    finally
+	    {
+	      rset.close();
+	    }
+	  }
+	  finally
+	  {
+	    stmt.close();
+	  }
+
+	  return res;
 	}
 }
