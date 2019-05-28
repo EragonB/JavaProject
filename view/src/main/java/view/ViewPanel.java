10613993e9c05debe5a3151a47419f3b0497c45a
sullyvanmaire <49433677+sullyvanmaire@users.noreply.github.com>
2019-05-28 09:14:59 +0200

    Commit Update Solve Error

--- view/src/main/java/view/ViewPanel.java
+++ view/src/main/java/view/ViewPanel.java
@@ -3,13 +3,7 @@ package view;
 import java.awt.Graphics;
 
 import java.awt.Image;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
 import javax.swing.JPanel;
-import PictureFactory.TradFactory;
 
 import FactoryPicture.MotherTradFactory;
 
