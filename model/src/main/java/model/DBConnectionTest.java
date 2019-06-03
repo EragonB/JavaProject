8b764f80fbb4c57c9b1d7476591c2445e34d4ed8
Marine-crty <mamaetlele@hotmail.fr>
2019-06-03 11:23:52 +0200

    Test of DBConnection

---
+++ model/src/main/java/model/DBConnectionTest.java
@@ -0,0 +1,112 @@
+/**
+ * 
+ */
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Marine-crty
+ *
+ */
+public class DBConnectionTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link model.DBConnection#getInstance()}.
+	 */
+	@Test
+	public void testGetInstance() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.DBConnection#getUrl()}.
+	 */
+	@Test
+	public void testGetUrl() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.DBConnection#setUrl(java.lang.String)}.
+	 */
+	@Test
+	public void testSetUrl() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.DBConnection#getLogin()}.
+	 */
+	@Test
+	public void testGetLogin() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.DBConnection#setLogin(java.lang.String)}.
+	 */
+	@Test
+	public void testSetLogin() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.DBConnection#getPassword()}.
+	 */
+	@Test
+	public void testGetPassword() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.DBConnection#setPassword(java.lang.String)}.
+	 */
+	@Test
+	public void testSetPassword() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.DBConnection#getConnection()}.
+	 */
+	@Test
+	public void testGetConnection() {
+		fail("Not yet implemented");
+	}
+
+}
